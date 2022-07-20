b91c1945f82428dca5486874a39c7befd5d842ab
Arobs100 <a.robs100@gmail.com>
2022-07-20 22:38:51 +1200

    Change title and add waves icon.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,14 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
   return (
     <nav>
-      <h1>Waves</h1>
+      <div style={{ display: "flex" }}>
+        <img
+          style={{ paddingRight: "1.5rem", width: "3.7rem" }}
+          src="../sound-wave.svg"
+          alt=""
+        />
+        <h1>Waves</h1>
+      </div>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Library
         <FontAwesomeIcon icon={faMusic} />
